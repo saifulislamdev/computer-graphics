c9f98996f70c7e00c02244597b0e1fcb32f5302c
Saiful Islam <nysaifulislam@gmail.com>
2022-03-02 00:45:16 -0500

    HW1a completed

--- hw1/HW1a.cpp
+++ hw1/HW1a.cpp
@@ -63,6 +63,10 @@ void
 HW1a::initializeGL()
 {
 	// PUT YOUR CODE HERE
+
+	// init state variables
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// set background color
+	glColor3f   (1.0F, 1.0f, 1.0f);		// set foreground color
 }
 
 
@@ -77,6 +81,25 @@ void
 HW1a::resizeGL(int w, int h)
 {
 	// PUT YOUR CODE HERE
+	m_winW = w;
+	m_winH = h;
+
+	// compute aspect ratio
+	float xmax, ymax;
+	float ar = (float) w / h;
+	if(ar > 1.0) {		// wide screen
+		xmax = ar;
+		ymax = 1.;
+	} else {		// tall screen
+		xmax = 1.;
+		ymax = 1/ar;
+	}
+
+	// init viewing coordinates for orthographic projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0);
 }
 
 
@@ -90,7 +113,30 @@ void
 HW1a::paintGL()
 {
 	// PUT YOUR CODE HERE
+	int gridX = 3;
+	int gridY = 3;
+	int viewportWidth = m_winW/gridX;
+	int viewportHeight = m_winH/gridY;
 
+	// clear canvas with background values
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	// display each P
+	for (size_t i = 0; i < gridX; i++) { // each row
+		for (size_t j = 0; j < gridY; j++) { // each column
+			int x = m_winW*j/3; // bottom-left x coordinate of viewport
+			int y = m_winH*i/3; // bottom-left y coordinate of viewport
+			glViewport(x, y, viewportWidth, viewportHeight); 
+			glBegin(DrawModes[i*3 + j]);
+			size_t verticesLength = sizeof(Vertices)/sizeof(Vertices[0]);
+			for (size_t k = 0; k < verticesLength; k+=2) { // draw vertex list
+				glVertex2f(Vertices[k], Vertices[k+1]); // draw each vertex
+			}
+			glEnd();
+		}
+	}
 }
 
 
