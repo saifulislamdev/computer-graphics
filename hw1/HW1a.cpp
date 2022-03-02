8407512557109c723611aa7d490e11320e56d826
ZiY1 <hzy1091149656@gmail.com>
2022-03-02 16:39:50 -0500

    Update HW1a.cpp

--- hw1/HW1a.cpp
+++ hw1/HW1a.cpp
@@ -62,7 +62,15 @@ HW1a::HW1a(const QGLFormat &glf, QWidget *parent)
 void
 HW1a::initializeGL()
 {
-	// PUT YOUR CODE HERE
+	// PUT YOUR CODE HERE -ziyi
+
+	// initialize GL function resolution for current context
+	initializeGLFunctions();
+
+	// init state variables
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// set background color
+	glColor3f(1.0F, 1.0f, 1.0f);		// set foreground color
+
 }
 
 
@@ -76,7 +84,32 @@ HW1a::initializeGL()
 void
 HW1a::resizeGL(int w, int h)
 {
-	// PUT YOUR CODE HERE
+	// PUT YOUR CODE HERE -ziyi
+
+	// assign private member m_winW and m_winH to the windows w and h
+	m_winW = w;
+	m_winH = h;
+
+	// compute aspect ratio
+	float xmax, ymax;
+	float ar = (float)w / h;
+	if (ar > 1.0) {		// wide screen
+		xmax = ar;
+		ymax = 1.;
+	}
+	else {		// tall screen
+		xmax = 1.;
+		ymax = 1 / ar;
+	}
+
+	// set viewport to occupy full canvas
+	glViewport(0, 0, w, h);
+
+	// init viewing coordinates for orthographic projection
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0);
 }
 
 
@@ -90,6 +123,38 @@ void
 HW1a::paintGL()
 {
 	// PUT YOUR CODE HERE
+	// clear canvas with background values
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// calculate the Vertices array length
+	int vertexArrLen = sizeof(Vertices) / sizeof(Vertices[0]);
+
+	int drawModeCursor = 0;
+
+	// iterate through a 3x3 matrix from left to right, bottom to top
+	for (int row = 0; row < 3; row++)
+	{
+		for (int col = 0; col < 3; col++)
+		{
+			// set the view port for each P
+			glViewport(col*m_winW / 3, row*m_winH / 3, m_winW / 3, m_winH / 3);
+
+			// draw the object
+
+			// define draw modes
+			glBegin(DrawModes[drawModeCursor]);
+			drawModeCursor++;
+
+			// iterate through the Vertices array from first item to second-to-last by icrementing 2
+			for (int j = 0; j < vertexArrLen - 1; j = j + 2) {
+				glVertex2f(Vertices[j], Vertices[j + 1]);
+			}
+
+			glEnd();
+		}
 
 }
 
