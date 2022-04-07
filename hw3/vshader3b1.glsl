31d5dc46838e946d4f9ccd8c12a08340c8071e9a
sanjidanisha <snisha000@citymail.cuny.edu>
2022-04-07 17:30:48 -0400

    3b

--- hw3/vshader3b1.glsl
+++ hw3/vshader3b1.glsl
@@ -1,8 +1,17 @@
 #version 330
 
 // PUT YOUR CODE HERE
+in	vec3	a_Position;	// attribute variable: position vector
+in	vec2	a_TexCoord;	// attribute variable: texture coordinate
+
+uniform   mat4  u_View;	// uniform variable for modelview matrix
+uniform   mat4  u_Projection;	// uniform variable for projection matrix
+
+out	vec2	v_TexCoord;	// varying variable for passing texture coordinate to fragment shader
 
 void main() 
 {
 	// PUT YOUR CODE HERE
+	gl_Position = u_Projection * u_View * vec4(a_Position, 1);
+	v_TexCoord = a_TexCoord;
 }
